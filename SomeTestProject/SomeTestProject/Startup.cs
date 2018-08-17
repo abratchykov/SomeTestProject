b3bd24f6da676cce7333cb397e34911b23e25f8a
abratchykov <abratchykov@morebis.pl>
2018-08-17 14:48:09 +0300

    Create project

---
+++ SomeTestProject/SomeTestProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SomeTestProject.Startup))]
+namespace SomeTestProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
