39a1aea1e51d6d83136adda465ec28b00f74aba6
abratchykov <abratchykov@morebis.pl>
2018-08-17 15:16:41 +0300

    Add NUnit tests project

---
+++ SomeTestProject/ClassLibrary1/FirstTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace NUnitTestsProject
+{
+    [TestFixture]
+    public class FirstTests
+    {
+
+        public FirstTests()
+        {
+        }
+
+        [Test]
+        public void IsTrueResultTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void IsFalseResultTest()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
